c0133b9b1ff7e244869a84ed1e6e73f3eb9bdd4c
thanhnd <ducthanhnguyen95@gmail.com>
2023-06-26 05:59:33 +0700

    Implementing authentication with form-based login

---
+++ src/main/java/com/example/authentication/with/form/based/login/AuthenticationWithFormBasedLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.authentication.with.form.based.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationWithFormBasedLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationWithFormBasedLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/authentication/with/form/based/login/AuthenticationWithFormBasedLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authentication.with.form.based.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationWithFormBasedLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
